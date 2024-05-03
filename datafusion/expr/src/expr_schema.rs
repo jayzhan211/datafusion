eeca54e2e23fadbcd2c63de6cdb3293fbfee719f
jayzhan211 <jayzhan211@gmail.com>
2024-05-03 13:42:20 +0800

    rm err
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>

--- datafusion/expr/src/expr_schema.rs
+++ datafusion/expr/src/expr_schema.rs
@@ -141,7 +141,7 @@ impl ExprSchemable for Expr {
                 match func_def {
                     ScalarFunctionDefinition::UDF(fun) => {
                         // verify that function is invoked with correct number and type of arguments as defined in `TypeSignature`
-                        data_types(&arg_data_types, fun.signature()).map_err(|err| {
+                        data_types(&arg_data_types, fun.signature()).map_err(|_| {
                             plan_datafusion_err!(
                                 "{}",
                                 utils::generate_signature_error_msg(
